30e1c798ce05699bfbee666121b44508db34996e
Vlad Firoiu <vladfi2@gmail.com>
2017-01-26 13:54:49 -0500

    Tweak default ssbm env.

--- dolphin/__init__.py
+++ dolphin/__init__.py
@@ -7,9 +7,13 @@ register(
     id='vladfi1/SSBM-v0',
     entry_point='dolphin:simpleSSBMEnv',
     reward_threshold=1,
+    timestep_limit=9999999,
     kwargs=dict(
       cpu=9,
       gui=True,
+      stage='battlefield',
+      mute=True,
+      speed=0,
     ),
     nondeterministic=True,
 )
